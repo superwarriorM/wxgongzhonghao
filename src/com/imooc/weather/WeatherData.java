ba38d764f292c757d1bb949f048fbb145605ab4d
yongfeim <myongfei@163.com>
2017-03-23 18:13:09 +0800

    weixin

---
+++ src/com/imooc/weather/WeatherData.java
@@ -0,0 +1,130 @@
+package com.imooc.weather;
+
+public class WeatherData {
+	
+	private String   city;//城市
+	private String   pinyin;//城市拼音
+	private String   citycode; //城市编码	
+	private String   date;//日期
+	private String   time;//发布时间
+	private String   postCode; //邮编
+	private String   longitude; //经度
+	private String   latitude; //维度
+	private String   altitude; //海拔	
+	private String   weather;  //天气情况
+	private String   temp; //气温
+	private String   l_tmp; //最低气温
+	private String   h_tmp; //最高气温
+	private String   WD;	 //风向
+	private String   WS; //风力
+	private String   sunrise; //日出时间
+	private String   sunset; //日落时间
+	
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getPinyin() {
+		return pinyin;
+	}
+	public void setPinyin(String pinyin) {
+		this.pinyin = pinyin;
+	}
+	public String getCitycode() {
+		return citycode;
+	}
+	public void setCitycode(String citycode) {
+		this.citycode = citycode;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public String getTime() {
+		return time;
+	}
+	public void setTime(String time) {
+		this.time = time;
+	}
+	public String getPostCode() {
+		return postCode;
+	}
+	public void setPostCode(String postCode) {
+		this.postCode = postCode;
+	}
+	public String getLongitude() {
+		return longitude;
+	}
+	public void setLongitude(String longitude) {
+		this.longitude = longitude;
+	}
+	public String getLatitude() {
+		return latitude;
+	}
+	public void setLatitude(String latitude) {
+		this.latitude = latitude;
+	}
+	public String getAltitude() {
+		return altitude;
+	}
+	public void setAltitude(String altitude) {
+		this.altitude = altitude;
+	}
+	public String getWeather() {
+		return weather;
+	}
+	public void setWeather(String weather) {
+		this.weather = weather;
+	}
+	public String getTemp() {
+		return temp;
+	}
+	public void setTemp(String temp) {
+		this.temp = temp;
+	}
+	public String getL_tmp() {
+		return l_tmp;
+	}
+	public void setL_tmp(String l_tmp) {
+		this.l_tmp = l_tmp;
+	}
+	public String getH_tmp() {
+		return h_tmp;
+	}
+	public void setH_tmp(String h_tmp) {
+		this.h_tmp = h_tmp;
+	}
+	public String getWD() {
+		return WD;
+	}
+	public void setWD(String wD) {
+		WD = wD;
+	}
+	public String getWS() {
+		return WS;
+	}
+	public void setWS(String wS) {
+		WS = wS;
+	}
+	public String getSunrise() {
+		return sunrise;
+	}
+	public void setSunrise(String sunrise) {
+		this.sunrise = sunrise;
+	}
+	public String getSunset() {
+		return sunset;
+	}
+	public void setSunset(String sunset) {
+		this.sunset = sunset;
+	}
+
+	
+	
+	
+
+}
---
+++ src/com/imooc/weather/WeatherInfo.java
@@ -0,0 +1,37 @@
+package com.imooc.weather;
+
+
+
+public class WeatherInfo {
+	
+	private String errNum;
+	private String errMsg;
+	private WeatherData retData; 
+	
+	public String getErrNum() {
+		return errNum;
+	}
+	public void setErrNum(String errNum) {
+		this.errNum = errNum;
+	}
+	public String getErrMsg() {
+		return errMsg;
+	}
+	public void setErrMsg(String errMsg) {
+		this.errMsg = errMsg;
+	}
+	public WeatherData getRetData() {
+		return retData;
+	}
+	public void setRetData(WeatherData retData) {
+		this.retData = retData;
+	}
+
+		
+
+
+
+
+	
+
+}
